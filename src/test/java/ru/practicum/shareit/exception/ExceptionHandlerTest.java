7993d249f537e38c5de5f9955f9ac0f0668e4c86
Kostya V <larind93@gmail.com>
2023-11-02 16:05:51 +0500

    Исправлены ошибки в тестах

--- src/test/java/ru/practicum/shareit/exception/ExceptionHandlerTest.java
+++ src/test/java/ru/practicum/shareit/exception/ExceptionHandlerTest.java
@@ -2,12 +2,20 @@ package ru.practicum.shareit.exception;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ru.practicum.shareit.booking.dto.BookingMapper;
+import ru.practicum.shareit.item.dto.ItemMapper;
+import ru.practicum.shareit.request.dto.ItemRequestMapper;
+import ru.practicum.shareit.user.dto.UserMapper;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class ExceptionHandlerTest {
     private final ErrorHandler errorHandler = new ErrorHandler();
+    private UserMapper userMapper;
+    private ItemMapper itemMapper;
+    private ItemRequestMapper itemRequestMapper;
+    private BookingMapper bookingMapper;
 
     @Test
     public void getTest() {
